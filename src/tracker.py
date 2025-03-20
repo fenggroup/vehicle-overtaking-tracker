6ec9ee95e47eb925bc3c13eb92b787a3cc2801c6
Gandhimathi Padmanaban <gmathi@umich.edu>
2025-03-20 06:48:25 -0400

    Updated Ground truth based on synchronized data, counter reset to 0, analysis jupyter notebook updated

--- src/tracker.py
+++ src/tracker.py
@@ -193,7 +193,7 @@ class VehiclePassTracker:
         sorted_image_sequence = get_sorted_images(self.image_sequence_path)
 
         # Initialize frame counter
-        current_frame = 1
+        current_frame = 0
         
         for image in sorted_image_sequence:
             
